c718ba6b4e0c6bf021335d333c8e72a718e08c8e
Rebeca Cesar <rebeca.costalenzcesa@ucalgary.ca>
2025-01-19 20:19:58 -0700

    texts

--- src/data/data.js
+++ src/data/data.js
@@ -4,8 +4,8 @@ const data = {
     name: "Rebeca Cesar",
     bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
     contact: {
-        email: "john.doe@gmail.com",
-        phone: "+12345678910",
+        email: "rebeca.costalenzcesa@ucalgary.com",
+        phone: "+5875008921",
         linkedin: "https://www.linkedin.com/",
         github: "https://www.github.com/",
     }
